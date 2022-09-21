f7a4fcecdcda24894e1328adc7b6234885d9d015
Willian <47341070+wjrcode@users.noreply.github.com>
2022-09-21 17:16:17 -0300

    dialog

--- lib/components/navigation/menu.dart
+++ lib/components/navigation/menu.dart
@@ -616,6 +616,52 @@ Future<DateTime?> veipls({
   );
 }
 
+Future<TimeOfDay?> showTimePicker({
+  required BuildContext context,
+  required TimeOfDay initialTime,
+  TransitionBuilder? builder,
+  bool useRootNavigator = true,
+  TimePickerEntryMode initialEntryMode = TimePickerEntryMode.dial,
+  String? cancelText,
+  String? confirmText,
+  String? helpText,
+  String? errorInvalidText,
+  String? hourLabelText,
+  String? minuteLabelText,
+  RouteSettings? routeSettings,
+  EntryModeChangeCallback? onEntryModeChanged,
+  Offset? anchorPoint,
+}) async {
+  assert(context != null);
+  assert(initialTime != null);
+  assert(useRootNavigator != null);
+  assert(initialEntryMode != null);
+  assert(debugCheckHasMaterialLocalizations(context));
+
+  final Widget dialog = TimePickerDialog(
+    initialTime: initialTime,
+    initialEntryMode: initialEntryMode,
+    cancelText: cancelText,
+    confirmText: confirmText,
+    helpText: helpText,
+    errorInvalidText: errorInvalidText,
+    hourLabelText: hourLabelText,
+    minuteLabelText: minuteLabelText,
+    onEntryModeChanged: onEntryModeChanged,
+  );
+  return showDialog<TimeOfDay>(
+    barrierDismissible: false,
+    barrierColor: Colors.white.withOpacity(0),
+    context: context,
+    useRootNavigator: useRootNavigator,
+    builder: (BuildContext context) {
+      return builder == null ? dialog : builder(context, dialog);
+    },
+    routeSettings: routeSettings,
+    anchorPoint: anchorPoint,
+  );
+}
+
 const List<String> list = <String>['baixa', 'média', 'alta'];
 
 class DropdownButtonExample extends StatefulWidget {
